f13fd3b43332d158dddb285d5214c55acf361b2e
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-17 15:07:47 -0300

    Add more aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,30 +1,44 @@
-# Enable aliases to be sudo’ed
-alias sudo='sudo '
+# Utils
+alias up='dot package update_all'
+alias sudo='sudo ' # Enable aliases to be sudo’ed
 
-alias ..="cd .."
-alias ...="cd ../.."
-alias ll="ls -l"
-alias la="ls -la"
-alias ~="cd ~"
+# Directory
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ~='cd ~'
+alias md='mkdir -p'
+alias rd='rmdir'
+
 alias dotfiles='cd $DOTFILES_PATH'
+alias sites='cd $HOME/Sites'
+
+alias ll='exa -l'
+alias la='exa -la'
 
 # Git
-alias gaa="git add -A"
-alias gc='$DOTLY_PATH/bin/dot git commit'
-alias gca="git add --all && git commit --amend --no-edit"
-alias gco="git checkout"
-alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
-alias gs="git status -sb"
-alias gf="git fetch --all -p"
-alias gps="git push"
-alias gpsf="git push --force"
-alias gpl="git pull --rebase --autostash"
-alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias ga='git add'
+alias gaa='git add -A'
+alias gc='git commit -m'
+# alias gc='$DOTLY_PATH/bin/dot git commit'
+alias gca='git add --all && git commit --amend --no-edit'
+alias gco='git checkout'
+alias gd='git diff'
+# alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
+alias gs='git status -sb'
+alias gf='git fetch --all -p'
+alias gps='git push'
+alias gpsf='git push --force'
+alias gpl='git pull --rebase --autostash'
+alias gb='git branch'
+alias gl='git log'
+# alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
-# Utils
-alias k='kill -9'
+# Editors
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
-alias o.='open .'
-alias up='dot package update_all'
+alias o.='open $PWD'
+alias v.='vim $PWD'
+
+# Processes
+alias k='kill -9'
+
