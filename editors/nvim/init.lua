11ddffd51446999f872e76588d680965a83e5607
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-21 22:17:11 -0300

    feat(editor): Reset and setup lazy loading for NeoVim

---
+++ editors/nvim/init.lua
@@ -0,0 +1,9 @@
+require("alpha")
+
+-- function()
+--   local text = "My dick is big, check it out"
+--   vim.api.nvim_buf_set_lines(0, -1, -1, true, {text})
+-- end
+-- vim.keymap.set("n", "<Space>a", require("alpha"))
+-- vim.api.nvim_create_user_command("Alpha", require("alpha"), {})
+
---
+++ editors/nvim/lua/alpha/init.lua
@@ -0,0 +1,4 @@
+require("alpha.settings")
+require("alpha.lazy")
+
+
