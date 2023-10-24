ce28e68bbaefbe1235b39a748ae6162cad683e9e
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-24 18:36:17 -0300

    feat(editor): Setup File Tree Navigator for NeoVim

---
+++ editors/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false,
+  config = function()
+		vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=None]])
+		require("nvim-tree").setup({
+			filters = {
+				dotfiles = false,
+			},
+		})
+	end,
+}
+
