62c35802fe6f5c5be43bf3739e794c76c9c70d70
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-24 02:21:30 -0300

    feat(editor): More Settings

--- editors/nvim/lua/alpha/settings.lua
+++ editors/nvim/lua/alpha/settings.lua
@@ -1,7 +1,9 @@
 vim.o.number = true
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 vim.g.mapleader = " "
 
 -- Compatibility for Notify plugin
 vim.o.termguicolors = true
+
+vim.o.colorcolumn = 80
 
