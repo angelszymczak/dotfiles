e1828fce9410e03ade89c8afa77fae737487e371
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-24 12:27:23 -0300

    some fixes

--- editors/nvim/lua/alpha/settings.lua
+++ editors/nvim/lua/alpha/settings.lua
@@ -1,9 +1,10 @@
+-- Globals
+
 vim.o.number = true
 vim.o.relativenumber = true
 vim.g.mapleader = " "
 
--- Compatibility for Notify plugin
-vim.o.termguicolors = true
+vim.o.termguicolors = true -- Required for: Notify, NeoSolarized theme
 
 vim.o.colorcolumn = 80
 
