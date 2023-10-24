5f89c58fc493a10a1fd4fe7b923d2029ae647212
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-24 11:30:10 -0300

    feat(editor): Set NeoSolarized theme for NeoVim

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,15 @@
 return {
-  "lifepillar/vim-solarized8",
-  branch = "neovim",
+  -- "lifepillar/vim-solarized8",
+  -- branch = "neovim",
+
+  "Tsuzat/NeoSolarized.nvim",
+  branch = "master",
 
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     vim.o.background = "dark"
-
-    -- load the Solarized colorscheme here
-    vim.cmd([[colorscheme solarized8_flat]])
+    vim.cmd([[colorscheme NeoSolarized]])
   end,
 }
 
