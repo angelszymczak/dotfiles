27c53812d1f86588e33477bfa6533c9520eaac9f
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-26 20:59:52 -0300

    feat(languages): Setup Node via ASDF

--- langs/js/setup.sh
+++ langs/js/setup.sh
@@ -1,7 +1,15 @@
 # Setup via ASDF
 
+## Dependencies
+brew install gawk
+brew install gpg
+
 ## Install the Plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
+## Setup Latest
+asdf install nodejs latest
 
+## Set Global version
+asdf global nodejs latest
 
