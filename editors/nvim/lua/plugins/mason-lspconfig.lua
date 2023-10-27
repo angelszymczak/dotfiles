2789d144e68a0f3b90be946b5e577bb2f4ee8844
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-27 11:14:30 -0300

    refact(editor): Set LSP, Fomatter and, Linter for Lua

--- editors/nvim/lua/plugins/mason-lspconfig.lua
+++ editors/nvim/lua/plugins/mason-lspconfig.lua
@@ -5,16 +5,89 @@ return {
   opts = {
     ensure_installed = {
       "efm",
-      "bashls",
-      "tsserver",
-      "solidity",
-      "tailwindcss",
-      "pyright",
-      "html",
-      "cssls",
+
+      -- Shell
+      -- "bashls",
+      -- "powershell_es",
+
+      -- Editor
+      -- "vimls",
+
+      -- Lua
       "lua_ls",
-      "emmet_ls",
-      "jsonls",
+
+      -- Web
+      -- "html",
+      -- "emmet_ls",
+      -- "cssls",
+      -- "tailwindcss",
+
+      -- JavaScript
+      -- "tsserver",
+      -- "vuels",
+
+      -- Data Mapping Standard
+      -- "jsonls"
+      -- "lemminx",
+      -- "yamlls",
+
+      -- Cucumber
+      -- "cucumber_language_server",
+
+      -- AWK
+      -- "awk_ls",
+
+      -- Arduino
+      -- "arduino_language_server",
+
+      -- Assembly
+      -- "asm_lsp",
+
+      -- C
+      -- "clangd",
+
+      -- Clojure
+      -- "clojure_lsp",
+
+      -- Docker
+      -- "dockerls",
+      -- "docker_compose_language_service"
+
+      -- SQL
+      -- "sqlls",
+
+      -- GraphQL
+      -- "graphql",
+
+      -- Go
+      -- "gopls"",
+
+      -- Python
+      -- "pyright",
+
+      --  Ruby
+      -- "ruby_ls",
+      -- "solargraph",
+      -- "sorbet",
+      -- "standardrb",
+
+      -- Rust
+      -- "rust_analyzer",
+
+      -- Smart Contracts
+      -- "solidity",
+
+      -- Terraform
+      -- "tflint",
+
+      -- Nix
+      -- "nix_ls",
+
+      -- Markdown
+      -- "marksman",
+
+      -- Java
+      -- "java_language_server",
     },
     automatic_installation = true,
   },
