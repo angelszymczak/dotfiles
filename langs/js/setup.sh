b180274311c75661c4278e6f5a242256d7b6c715
Angel Szymczak <angel.szymczak@hotmail.com>
2023-10-28 18:44:58 -0300

    fix(lang): Setup Nodejs via ASDF

--- langs/js/setup.sh
+++ langs/js/setup.sh
@@ -7,9 +7,10 @@ brew install gpg
 ## Install the Plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
+lastest="21.1.0"
 ## Setup Latest
-asdf install nodejs latest
+asdf install nodejs ${latest}
 
 ## Set Global version
-asdf global nodejs latest
+asdf global nodejs ${latest}
 
