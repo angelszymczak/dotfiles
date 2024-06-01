89fc28036b969eddb9db164b1e29d30d0516f6f7
Angel Szymczak <angel.szymczak@hotmail.com>
2024-06-01 20:55:31 -0300

    fix: Add setup lang sample for Ruby

--- langs/ruby/setup.sh
+++ langs/ruby/setup.sh
@@ -10,6 +10,5 @@ asdf install ruby 3.3.0-preview2
 asdf global ruby 3.3.0-preview2
 
 # To create the .tool-versions
- asdf install ruby 3.2.2 
+# asdf install ruby 3.2.2
 # asdf local ruby 2.2.2 
-
