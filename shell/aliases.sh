7d3d31555d7a1b00af610bd8b7c18d58d4e1b474
Angel Szymczak <angel.szymczak@hotmail.com>
2024-06-03 13:08:21 -0300

    feat: setup Bat for better Cat

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -25,6 +25,16 @@ alias ls="eza --color=always --long --all --git --no-filesize --icons=always --n
 alias ls1="ls --tree --level=1"
 alias ls2="ls --tree --level=2"
 
+# ------------------------------------------------------------------------------
+# ---- Bat (beter cat) ---------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+#  Check themes in your directory
+#  don't forget to clone bat_theme config, check it out in README.md file
+#  bat --list-themes | fzf --preview="bat --theme={} --color=always <PATH_TO_YOUR_SAMPLE_VIEW>"
+
+export BAT_THEME="tokyonight_night"
+
 
 # ------------------------------------------------------------------------------
 # Git
