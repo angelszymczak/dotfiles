ff59d853aa27f08f1710438b1fcfc98912f3dce3
Angel Szymczak <angel.szymczak@hotmail.com>
2024-06-09 22:14:17 -0300

    feat: set up work Spaces for Menu at Sketchybar

--- menu-bar/sketchybar/items/spaces.sh
+++ menu-bar/sketchybar/items/spaces.sh
@@ -4,13 +4,13 @@ SPACE_SIDS=(1 2 3 4 5 6 7 8 9 10)
 
 for sid in "${SPACE_SIDS[@]}"
 do
-  sketchybar --add space space.$sid left                                    \
-             --set space.$sid space=$sid                                    \
-                              icon=$sid                                     \
-                              label.font="sketchybar-app-font:Regular:16.0" \
-                              label.padding_right=20                        \
-                              label.y_offset=-1                             \
-                              script="$SKETCHY_PLUGINS_DIR/space.sh"
+  sketchybar --add space space."$sid" left                                    \
+             --set space."$sid" space="$sid"                                  \
+                                icon="$sid"                                   \
+                                label.font="sketchybar-app-font:Regular:16.0" \
+                                label.padding_right=20                        \
+                                label.y_offset=-1                             \
+                                script="$SKETCHY_PLUGINS_DIR/space.sh"
 done
 
 sketchybar --add item space_separator left                                      \
@@ -21,3 +21,5 @@ sketchybar --add item space_separator left                                      
                                  background.drawing=off                         \
                                  script="$SKETCHY_PLUGINS_DIR/space_windows.sh" \
            --subscribe space_separator space_windows_change
+
+
