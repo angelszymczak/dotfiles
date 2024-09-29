563823cb9a16bf44d96bdca0586f31714b4b6f07
Angel Szymczak <angel.szymczak@hotmail.com>
2024-09-29 16:54:21 -0300

    feat(editor): integrate Git to NeoVim by Fugitive

--- editors/nvim/lua/angel/plugins/fugitive.lua
+++ editors/nvim/lua/angel/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
   "tpope/vim-fugitive",
+  cmd = { "G", "Git" },
+  keys = {
+    { "<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch" },
+    { "<leader>gk", ":Git fetch --all -p<cr>", desc = "Git fetch" },
+  },
 }
